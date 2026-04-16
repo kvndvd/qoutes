63d076cac2c031b846dba10e7a73a57a49d647b0
Kevin Belnas <kevindavid0325@gmail.com>
2026-04-16 16:41:49 +0800

    update code in upload_to_s3

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,18 +1,10 @@
-import os
 import boto3
 
-def upload_csv():
-    bucket_name = os.environ.get("bucketqoutes100")
-    file_path = "output/quotes.csv"
-    s3_key = "quotes/quotes.csv"
+file_path = "output/quotes.csv"
+bucket_name = "bucketqoutes100"
+s3_key = "output/quotes.csv"
 
-    if not bucket_name:
-        raise ValueError("bucketqoutes100 is not set")
+s3 = boto3.client("s3")
+s3.upload_file(file_path, bucket_name, s3_key)
 
-    s3 = boto3.client("s3")
-    s3.upload_file(file_path, bucket_name, s3_key)
-
-    print(f"Uploaded {file_path} to s3://{bucket_name}/{s3_key}")
-
-if __name__ == "__main__":
-    upload_csv()
+print("Upload successful")
