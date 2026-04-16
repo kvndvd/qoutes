f647c04c9ffd2220c97d9d2e033e58f8f0173b28
Kevin Belnas <kevindavid0325@gmail.com>
2026-04-16 15:44:09 +0800

    update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ BASE_URL = "https://quotes.toscrape.com/"
 def session():
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     return driver
 
