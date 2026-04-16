0b4b9312ad04dd6090d9d21924c8c663cf593c6e
Kevin Belnas <kevindavid0325@gmail.com>
2026-04-16 18:13:05 +0800

    removal of requirement.tx

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ BASE_URL = "https://quotes.toscrape.com/"
 
 def session():
     options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--headless")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     return driver
 
